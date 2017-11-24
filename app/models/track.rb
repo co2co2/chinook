38ad76209428f363104be1b0c319ebb1c6b0a535
co2co2 <coco8287@hotmail.com>
2017-11-24 10:38:40 -0500

    new methods for track

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,34 @@
 class Track < ApplicationRecord
+
+  def self.short
+    shorter_than(SHORT)
+  end
+  def self.medium
+    longer_than_or_equal_to(SHORT).shorter_than(LONG)
+  end
+
+  def self.long
+    longer_than_or_equal_to(LONG)
+  end
+
+  def self.shorter_than(milliseconds)
+    if milliseconds && milliseconds>0
+      where("milliseconds<?", milliseconds)
+    else
+      all
+    end
+  end
+
+  def self.longer_than_or_equal_to(milliseconds)
+    if milliseconds && milliseconds>0
+      where("milliseconds>=?", milliseconds)
+    else
+      all
+    end
+  end
+  
+  def self.starts_with(char)
+    where("name ILIKE ?","#{char}%")
+  end
+
 end
